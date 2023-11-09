6fb52322d08887679d51e2c064536beac7a54ae2
ZulfiyyaQ <tu7u71zbq@code.edu.az>
2023-11-09 20:40:24 +0400

    modified task

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,14 @@ var row = document.getElementsByClassName("row")[0]
 function movieapp(data) {
     data.forEach(datas => {
         row.innerHTML += `
-        <div class="col-4 col;lg-3">
+        <div class="col-4 col-lg-3">
     <div class="card" ">
     <img src="${datas.image.medium}" class="card-img-top" alt="...">
     <div class="card-body">
+    
       <h5 class="card-title">${datas.name}</h5>
-      <p class="card-text">${datas.rating.average}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      
+      <a href="#" </a>
     </div>
   </div>
 </div>`
@@ -19,3 +20,4 @@ function movieapp(data) {
 fetch("https://api.tvmaze.com/shows").then(response=>response.json()).then(data=>movieapp(data))
 
 
+{/* <p class="card-text">${datas.rating.average}</p> */}
